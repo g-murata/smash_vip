a58bcbf1e53d6e376e70fd544edb391c02c7ea74
ta-mu-g-m <jojo1234dorararara@gmail.com>
2023-01-28 15:04:11 +0900

    ADD_モーダルボタン

---
+++ src/components/SampleModal.jsx
@@ -0,0 +1,53 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+function SampleModal() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello Modal</h2>
+        <button onClick={closeModal}>close</button>
+        <div>モーダル</div>
+        <form>
+          <input />
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
+export default SampleModal;
